7a6d26eca605255e29bd34ac8082da02fbf45ccb
Nader Nehal <nehalsikdar93@gmail.com>
2025-02-12 22:24:15 -0500

    Created PDFDocument model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class PDFDocument(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+
+    # change pdf file location
+    pdf_file = models.FileField(upload_to='pdfs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
