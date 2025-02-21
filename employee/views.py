c08737fc623cd589f6281a6bedd6382c49bbaa5a
Nader Nehal <nehalsikdar93@gmail.com>
2025-02-20 23:53:02 -0500

    Created register view and template

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'employee/register.html', {"form": form})
