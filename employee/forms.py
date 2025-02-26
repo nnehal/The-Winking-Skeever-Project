4f7ae9697a1e434ebbe00d33b0604201f7602c2e
Nader Nehal <nehalsikdar93@gmail.com>
2025-02-25 22:11:26 -0500

    Updated UserRegistrationForm

--- employee/forms.py
+++ employee/forms.py
@@ -4,12 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    id = forms.IntegerField()
-    ssn = forms.IntegerField()
-    phone = forms.IntegerField()
-    emergency_contact = forms.IntegerField()
-    picture = forms.ImageField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'id', 'ssn', 'phone', 'emergency_contact', 'picture']
+        fields = ['username', 'email', 'password1', 'password2']
