154322dd10b0c6cd5cbe274616dab7d4fdd7f89d
Nader Nehal <nehalsikdar93@gmail.com>
2025-02-27 18:26:17 -0500

    Added new functionality to profile page

--- employee/views.py
+++ employee/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .forms import UserRegisterForm
-# Create your views here.
+from post.models import UploadDocument
+import csv
 
 def register(request):
     if request.method == "POST":
@@ -19,4 +21,49 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "employee/profile.html")
+    # csv file name
+    filename = UploadDocument.objects.last().File.url
+
+    # initializing the titles and rows list
+    # fields = []
+    # rows = []
+    name = User.username
+    schedule = []
+    # reading csv file
+    with open(filename) as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+
+        for row in csvreader:
+            for i in range(len(row)):
+                if(name == row[i].strip().title()):
+                    time = row[0]
+                    match(i):
+                        case 1:
+                            schedule.append(time + " Monday")
+                        case 2:
+                            schedule.append(time + " Tuesday")
+                        case 3:
+                            schedule.append(time + " Wednesday")
+                        case 4:
+                            schedule.append(time + " Thursday")
+                        case 5:
+                            schedule.append(time + " Friday")
+                        case 6:
+                            schedule.append(time + " Saturday")
+                        case 7:
+                            schedule.append(time + " Sunday")
+                        case _:
+                            print("Go HOME!!\n")
+
+            
+
+    context = {
+        "schedule": schedule
+    }
+    return render(request, "employee/profile.html", context)
