8d2ccc212e686d229d2b0b2cd6d227c28e9f6680
Nader Nehal <nehalsikdar93@gmail.com>
2025-05-22 21:48:28 -0400

    Merged Post and Schedule

--- post/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UploadDocument
-
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = UploadDocument
-        fields = ["Description", "File"]
--- post/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class UploadDocument(models.Model):
-    Description = models.CharField(max_length=255, blank=True)
-    File = models.FileField(upload_to="sched/")
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.Description
